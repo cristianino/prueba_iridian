fb913b6804d1dbdab9712cf851c8c80eefdd3efe
Cristian Niño <cristiandavid113@gmail.com>
2023-11-30 09:35:25 -0500

    chore: save contacto data

--- src/Controller/ContactoController.php
+++ src/Controller/ContactoController.php
@@ -8,7 +8,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ContactoController extends AbstractController
 {
@@ -27,14 +29,11 @@ class ContactoController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Obtener el correo electrónico del formulario
             $correo = $form->get('correo')->getData();
 
-            // Obtener la fecha actual sin hora
             $fechaHoy = new \DateTime();
             $fechaHoy->setTime(0, 0, 0);
 
-            // Verificar si ya existe un mensaje de este correo en la fecha actual
             $mensajeExistente = $this->entityManager->getRepository(Contacto::class)
                 ->findOneBy([
                     'correo' => $correo,
@@ -42,22 +41,51 @@ class ContactoController extends AbstractController
                 ]);
 
             if ($mensajeExistente) {
-                // Agregar un mensaje de error si ya se envió un mensaje hoy
                 $this->addFlash('error', 'Ya has enviado un mensaje hoy.');
             } else {
-                // Si no hay mensajes previos en la fecha actual, procesar el nuevo mensaje
                 $contacto->setFechaEnvio(new \DateTime());
                 $this->entityManager->persist($contacto);
                 $this->entityManager->flush();
 
-                // Redirigir o mostrar un mensaje de éxito
                 $this->addFlash('success', 'Tu mensaje ha sido enviado con éxito.');
-                //return $this->redirectToRoute('inicio');
             }
         }
 
         return $this->render('contacto/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/api/contacto', name: 'api_contacto', methods: ['POST'])]
+    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
+    {
+        try {
+            $contacto = $serializer->deserialize($request->getContent(), Contacto::class, 'json');
+
+            $correo = $contacto->getCorreo();
+            $fechaActual = new \DateTime();
+            $contactoExistente = $this->entityManager->getRepository(Contacto::class)
+                ->findOneBy([
+                    'correo' => $correo,
+                    'fechaEnvio' => $fechaActual
+                ]);
+
+            if ($contactoExistente) {
+                return $this->json(['error' => 'Solo puedes enviar un mensaje por día.'], Response::HTTP_BAD_REQUEST);
+            }
+
+
+            $errors = $validator->validate($contacto);
+            if (count($errors) > 0) {
+                return $this->json($errors, Response::HTTP_BAD_REQUEST);
+            }
+
+            $entityManager->persist($contacto);
+            $entityManager->flush();
+
+            return $this->json($contacto, Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
     }
 }
